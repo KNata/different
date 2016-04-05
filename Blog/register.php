9831306ca49452b763737bba917abd801c6b6b00
Natalia <nataliakiselyk@gmail.com>
2016-04-05 19:11:08 +0300

    add style to register.php

--- Blog/register.php
+++ Blog/register.php
@@ -1,46 +1,47 @@
 <html>
-	<head>
+<head>
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 
 
-		<title>Blog</title>
-	</head>
-	<body>
-		<h2>Registration</h2>
-		<a href="index.php">Return</a><br/><br/>
-		<form action="register.php" method="post">
-			Username: <input type="text" name="username" required="required"/> <br/>
-			Password: <input type="password" name="password" required="required" /> <br/>
-			<input type="submit" value="Register"/>
-		</form>
-	</body>
+<title>Blog</title>
+</head>
+<body>
+<legend><h2>Registration</h2></legend>
+<fieldset>
+
+<a href="index.php">Return</a><br/><br/>
+<form action="register.php" method="post">
+Username:   <input type="text" name="username" required="required"/> <br/><br/>
+Password:   <input type="password" name="password" required="required" /> <br/>
+<br>
+<input type="submit" value="Register"/>
+</form>
+</fieldset>
+</body>
 </html>
 
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-	$username = mysql_real_escape_string($_POST['username']);
-	$password = mysql_real_escape_string($_POST['password']);
-    $bool = true;
-
-	mysql_connect("localhost", "root","root") or die(mysql_error());
-	mysql_select_db("first_db") or die("Cannot connect to database");
-	$query = mysql_query("Select * from users");
-	while($row = mysql_fetch_array($query))
-	{
-		$table_users = $row['username'];
-        if($username == $table_users) {
-			$bool = false;
-			Print '<script>alert("Username has been taken!");</script>';
-			Print '<script>window.location.assign("register.php");</script>'; 		}
-	}
-
-	if($bool) {
-		mysql_query("INSERT INTO users (username, password) VALUES ('$username','$password')");  		Print '<script>alert("Successfully Registered!");</script>';
-		Print '<script>window.location.assign("register.php");</script>';
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = mysql_real_escape_string($_POST['username']);
+        $password = mysql_real_escape_string($_POST['password']);
+        $bool = true;
+        mysql_connect("localhost", "root","root") or die(mysql_error());
+        mysql_select_db("first_db") or die("Cannot connect to database");
+        $query = mysql_query("Select * from users");
+        while($row = mysql_fetch_array($query))
+        {
+            $table_users = $row['username'];
+            if($username == $table_users) {
+                $bool = false;
+                Print '<script>alert("Username has been taken!");</script>';
+                Print '<script>window.location.assign("register.php");</script>'; 		}
+        }
+        if($bool) {
+            mysql_query("INSERT INTO users (username, password) VALUES ('$username','$password')");  		Print '<script>alert("Successfully Registered!");</script>';
+            Print '<script>window.location.assign("register.php");</script>';
+        }
     }
-
-}
-?>
+    ?>
